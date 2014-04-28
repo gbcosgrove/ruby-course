3c9f7d7802c2d89844722ee07635cc16bee71417
Greg Cosgrove <gregcosgrove@gmail.com>
2014-04-28 14:51:42 -0500

    Add test -- returns number of elements in the array

--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -12,3 +12,10 @@ describe 'Exercise 0' do
     expect(result).to eq("nope")
   end
 end
+
+describe 'Exercise 1' do
+  it "returns the number of elements in the array" do
+    array_count = Exercises.ex1([1, 2, 3, 4, 5])
+    expect(result).to eq(5)
+  end
+end

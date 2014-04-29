b5bd2eb408756d21fe88a61ee86ae1e2ee48abd1
Greg Cosgrove <gregcosgrove@gmail.com>
2014-04-29 08:58:35 -0500

    set up first 9 test with no parameters

--- exercises.rb
+++ exercises.rb
@@ -14,13 +14,13 @@ module Exercises
   # Exercise 1
   #  - Returns the number of elements in the array
   def self.ex1(array)
-    array.length
+
   end
 
   # Exercise 2
   #  - Returns the second element of an array
   def self.ex2(array)
-    # TODO
+
   end
 
   # Exercise 3
--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -1,5 +1,5 @@
 require "./exercises.rb"
-#require 'spec_helper'
+#require "spec_helper"
 
 describe 'Exercise 0' do
   it "triples the length of a string" do
@@ -15,15 +15,52 @@ end
 
 describe 'Exercise 1' do
   xit "returns the number of elements in the array" do
-    array = Exercises.ex1([1, 2, 3, 4, 5])
-    expect(array.length).to eq(5)
+    result = Exercises.ex1 [1]
+    expect result.length.to eq(1)
+
   end
 end
 
+describe 'Exercise 2' do
+  xit "returns the second element of an array" do
+    result = Exercises.ex2([1,2,3,4,5])
+    expect(result[1]).to eq(2)
+  end
+end
+
+describe 'Exercise 3' do
+  xit 'returns the sume of the given array of numbers' do
+  end
+end
+
+describe 'Exercise 4' do
+  xit 'Returns the max number of the given array of numbers' do
+  end
+end
 
 describe '.ex5' do
   it "should call puts with the element of a 1 element array" do
   STDOUT.should_receive(:puts).with(10)
   Exercises.ex5([10])
+  end
+end
+
+describe 'Exercise 6' do
+  xit 'updates the last it in the array to panda' do
+  end
+end
+
+describe 'Exercise 7' do
+  xit 'adds str to the array if str does not exist' do
+  end
+end
+
+describe 'Exercise 8' do
+  xit "it iterates through hash 'people and prints out their name and occupation" do
+  end
+end
+
+describe 'Exercise 9' do
+  xit 'it returns ture if the given time is in a leap year' do
   end
 end
